09687cdbcca40a0f4c5e171ecc65314311ebaa49
LEl_FENG <89715290+1x000@users.noreply.github.com>
2023-03-10 20:26:00 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,13 +11,13 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/128.png',
+  icon: 'cdn.jsdelivr.net/gh/1x000/zdzy/tp/avatar.jpg',
 
   // Prefix for KV Storage
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "LEl_FENG 资源站2",
+  title: "LEl_FENG 资源站 大文件",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://blog.xpdbk.com/" target="_blank" rel="noopener noreferrer">myblog</a>. 我的博客 .',
+    'Powered by <a href="https://blog.xpdbk.com/" target="_blank" rel="noopener noreferrer">LEl_FENG</a>.  .',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
